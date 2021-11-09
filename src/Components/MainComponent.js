df444df4763eb3ecc4f7601074b2a1b9611e374b
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-11-09 16:49:34 -0600

    changed HeaderComponent and MainComponent to functional components, learning hooks

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './HeaderComponent';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-    }
+function Main() {
     
-    render() {
         return (
             <React.Fragment>
                 <Header />
             </React.Fragment>
         )
     }
-}
 
 export default Main;
--- src/Components/delete.js
+++ src/Components/delete.js
@@ -2,30 +2,30 @@ import React, { useState } from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, NavLink } from 'reactstrap';
 
 function Header() {
-    const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(true);
     const toggleNav = () => setCollapsed(!collapsed);
 
     return (
         <React.Fragment>
-            <Navbar color="dark" sticky="top" expand="md" className="nav-bar">  
+            <Navbar dark sticky="top" expand="md" className="nav-bar">  
                 <div className="container">
                     <NavbarBrand href="/" className="NavbarBrand font-weight-bold"><ion-icon name="leaf" /> 
                     Forever Creating!</NavbarBrand> 
-                    <NavbarToggler onClick={toggleNav} />
-                    <Collapse isOpen={collapsed} navbar>
+                    <NavbarToggler onClick={this.toggleNav} />
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
                         <Nav navbar className="ml-5">
                             <NavItem>
-                                <NavLink className="nav-link mr-4" to="/home" onClick={toggleNav}>
+                                <NavLink className="nav-link mr-4" to="/home" onClick={this.toggleNav}>
                                 <ion-icon name="home" />  &nbsp;HOME
                                 </NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link mr-4" to="/projects" onClick={toggleNav}>
+                                <NavLink className="nav-link mr-4" to="/projects" onClick={this.toggleNav}>
                                 <ion-icon name="documents" /> &nbsp;SHOP
                                 </NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link" to="/contact" onClick={toggleNav}>
+                                <NavLink className="nav-link" to="/contact" onClick={this.toggleNav}>
                                 <ion-icon name="call" /> &nbsp;CONTACT
                                 </NavLink>
                             </NavItem>
