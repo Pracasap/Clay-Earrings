ee4c823689793ed0dc5195c0086bd3ff6b6018e4
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-11-15 19:09:36 -0600

    added favicon

---
+++ src/assets/clayEarrings.js
@@ -0,0 +1,83 @@
+export const Earrings = [
+    {
+        "id": 0,
+        "name": "Portfolio Mobile App",
+        "image": "images/portfolio-React-native.png",
+        "website": false,
+        "github": "https://github.com/Pracasap/Portfolio-react-native",
+        "codepen": false,
+        "description": "This is my personal portfolio mobile app that I built using React Native. I learnt different React Native UI Components, React Native Alerts, Animations, Gestures, The Expo SDK, and much more while working on this project."
+    },
+    {
+        "id": 1,
+        "name": "Personal Portfolio",
+        "image": "images/portfolio.png",
+        "website": "https://psapkota.com",
+        "github": "https://github.com/Pracasap/Portfolio-react-redux",
+        "codepen": false,
+        "description": "This is my personal portfolio webpage. I used Bootstrap, Reactstrap, React, React-redux, and many other tools to build this app. I had a blast working on this project and projects like this make my journey worthwhile."
+    },
+    {
+        "id": 2,
+        "name": "FineDine",
+        "image": "images/fineDine.png",
+        "website": "https://finedine.netlify.app",
+        "github": "https://github.com/Pracasap/FineDine",
+        "codepen": false,
+        "description": "FineDine is a bootstrap project I worked on during Nucamp Bootcamp. I learned about bootstrap containers, modals, accordion, and a whole lot more bootstrap classes working on this project. I had so much fun."
+    },
+    {
+        "id": 3,
+        "name": "Nucamp",
+        "image": "images/nucamp.png",
+        "website": "https://nucampreactredux.netlify.app",
+        "github": "https://github.com/Pracasap/React-Nucamp",
+        "codepen": false,
+        "description": "NuCamp is a webpage I build with React when I was in the Nucamp Bootcamp. I learned about React, Reactstrap, Single Page Applications(SPA), React Router, Redux, etc. It was challenging but I know that is where the growth is."
+    },
+    {
+        "id": 4,
+        "name": "Tribute Page",
+        "image": "images/tribute.png",
+        "website": "https://pracasap.github.io/Tribute-page/",
+        "github": "https://github.com/Pracasap/Tribute-page",
+        "codepen": "https://codepen.io/prakash-sapkota/pen/QBBLWm",
+        "description": "I made a simple HTML, CSS webpage tributing the Nobel Peace Prize winner Dr. Norman Borlaug. Having a strong foundation in HTML and CSS by working on projects like this really helped me learn Bootstrap quickly."
+    },
+    {
+        "id": 5,
+        "name": "Survey Form",
+        "image": "images/surveyform.png",
+        "website": "https://pracasap.github.io/Survey-Form/",
+        "github": "https://github.com/Pracasap/Survey-form",
+        "codepen": "https://codepen.io/prakash-sapkota/pen/rNMYxYj",
+        "description": "Survery Form is a webpage I built when I was learning about forms. It was a small project, yet I got to learn about background image, opacity, different form tags and attributes while having a lot of fun on top of that."
+    },
+    {
+        "id": 6,
+        "name": "Old Portfolio",
+        "image": "images/pracasap.github.io.png",
+        "website": "https://pracasap.github.io",
+        "github": "https://github.com/Pracasap/pracasap.github.io",
+        "codepen": "https://codepen.io/prakash-sapkota/pen/LYRBeKP",
+        "description": "This was my previous personal portfolio webpage. It is a simple responsive webpage I built using HTML and CSS only. This webpage definitely gave me ideas and inspiration to build a better personal portfolio webpage."
+    },
+    {
+        "id": 7,
+        "name": "Product Landing Page",
+        "image": "images/landingpage.png",
+        "website": "https://pracasap.github.io/Product-Langing-Page/",
+        "github": "https://github.com/Pracasap/Product-Langing-Page",
+        "codepen": "https://codepen.io/prakash-sapkota/pen/WNGXaYO",
+        "description": "This project taught me that a simple webpage like this can look great. It had very basic colors yet looked awesome. I also got to learn how to make containers and cards using CSS. Furthermore, I learned about grid and flexbox in CSS."
+    },
+    {
+        "id": 8,
+        "name": "Technical Documentation",
+        "image": "images/technicaldocumentation.png",
+        "website": "https://pracasap.github.io/Technical-Documentation/",
+        "github": "https://github.com/Pracasap/Technical-Documentation",
+        "codepen": "https://codepen.io/prakash-sapkota/pen/OJRvLwK",
+        "description": "This project not only taught me about grid and flexbox, but made me wanna learn JavaScript. Working on this project, I realized how much I love making webpages and that is when I decided to focus on learning web development."
+    }
+];
